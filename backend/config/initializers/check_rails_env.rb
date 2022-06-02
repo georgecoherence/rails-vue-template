e3ece3d05d9f14c3550401f3a29f8407cc1769a7
Adam Abdelaziz <adam.abdelaziz@withcoherence.com>
2022-06-02 21:31:21 +0000

    Update rails initializers

--- backend/config/initializers/check_rails_env.rb
+++ backend/config/initializers/check_rails_env.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-if ENV['COHERENCE_DEV'].blank?
+if ENV['COHERENCE_DEV']
+    Rails.env = "development"
+elsif ENV['COHERENCE_TEST']
+    Rails.env = "test"
+else
     Rails.env = "production"
 end
