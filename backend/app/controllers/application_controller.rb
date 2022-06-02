6f225aa2ebfd9f811d47bbfcaa461b3a2afd4484
Julie Swoope <julie.swoope@withcoherence.com>
2022-06-02 20:33:58 +0000

    add index controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    def index
+        cats = Cat.all
+        names = cats.map{ |cat| cat.name }
+        render :json => {:names => names}  
+    end
 end
